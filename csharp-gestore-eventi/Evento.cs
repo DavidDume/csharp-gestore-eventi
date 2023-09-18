78118ed9620400c5cf979591ed7cfe739a145bab
David Dume <dumedavid11@gmail.com>
2023-09-18 10:27:45 +0200

    classe Evento

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class Evento
+    {
+        public string titolo { get; set; }
+        public DateTime Data { get; set; }
+        public int CapienzaMax { get; }
+        public int PostiPrenotati { get; }
+
+        public String Titolo
+        {
+            get { return titolo; }
+            set
+            {
+                if(titolo == "")
+                {
+                    return;
+                }
+                titolo = value;
+            }
+        
+
+    }
+}
