e4767ac2d8d9aab3d439f3fc4386150bb1973ed2
David Dume <dumedavid11@gmail.com>
2023-09-18 10:48:26 +0200

    completato la classe evento

--- csharp-gestore-eventi/Evento.cs
+++ csharp-gestore-eventi/Evento.cs
@@ -9,23 +9,101 @@ namespace csharp_gestore_eventi
 {
     public class Evento
     {
-        public string titolo { get; set; }
-        public DateTime Data { get; set; }
-        public int CapienzaMax { get; }
-        public int PostiPrenotati { get; }
+        private string titolo;
+        private DateTime data;
+        private int capienzaMax;
+        private int postiPrenotati;
 
-        public String Titolo
+        public Evento(string titolo, DateTime data, int capienzaMassima)
         {
-            get { return titolo; }
+            Titolo = titolo;
+            Data = data;
+            this.capienzaMax = capienzaMassima;
+            this.postiPrenotati = 0;
+        }
+
+        public string Titolo
+        {
+            get
+            {
+              return titolo;
+            }
             set
             {
-                if(titolo == "")
+                if (value == "")
                 {
-                    return;
+                    throw new Exception("Il titolo non può essere vuoto");
                 }
                 titolo = value;
             }
-        
+        }
 
+        public DateTime Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                if (value < DateTime.Now)
+                {
+                    throw new Exception("La data non può essere già passata");
+                }
+                data = value;
+            }
+        }
+
+        public int CapienzaMassima { 
+            get
+            {
+                return capienzaMax;
+            }
+            
+        }
+
+        public int PostiPrenotati
+        {
+            get
+            {
+                return postiPrenotati;
+            }
+        }
+
+        public void PrenotaPosti(int postiDaPrenotare)
+        {
+            if (data < DateTime.Now)
+            {
+                throw new Exception("L'evento è già passato");
+            }
+
+            if (postiDaPrenotare > capienzaMax - postiPrenotati)
+            {
+                throw new Exception("Non ci sono abbastanza posti disponibili");
+            }
+
+            postiPrenotati += postiDaPrenotare;
+        }
+
+        public void DisdiciPosti(int postiDaDisdire)
+        {
+            if (data < DateTime.Now)
+            {
+                throw new Exception("L'evento è già passato");
+            }
+
+            if (postiDaDisdire > postiPrenotati)
+            {
+                throw new Exception("Non ci sono abbastanza posti prenotati da disdire");
+            }
+
+            postiPrenotati -= postiDaDisdire;
+        }
+
+        public override string ToString()
+        {
+            return $"{Data.ToString("dd/MM/yyyy")} – {Titolo}";
+        }
     }
+
 }
