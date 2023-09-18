cdee113ac730e39227a4e6523b95aa17e4eb681b
David Dume <dumedavid11@gmail.com>
2023-09-18 11:44:35 +0200

    creazione calsse programma eventi

---
+++ csharp-gestore-eventi/ProgrammaEvento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgrammaEventi
+    {
+        public string titolo {  get; set; }
+        public List<Evento> eventi { get; private set;}
+
+        public ProgrammaEventi(string titolo)
+        {
+            this.titolo = titolo;
+            eventi = new List<Evento>();
+        }
+
+        public void AggiungiEvento(Evento evento)
+        {
+            eventi.Add(evento);
+        }
+
+    }
+}
